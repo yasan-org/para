0943af58cd90b5d57130a1a4d4c2448c3b8f349d
Yasan <yasan@yasan.dev>
2022-09-15 20:01:14 +0430

    Create username preview providers

---
+++ para/src/main/kotlin/dev/yasan/para/string/username/ParaStringUsernameWithAtSign.kt
@@ -0,0 +1,18 @@
+package dev.yasan.para.string.username
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class ParaStringUsernameWithAtSign : PreviewParameterProvider<String> {
+    override val values: Sequence<String> = sequenceOf(
+        "@selenagomez",
+        "@kyliejenner",
+        "@therock",
+        "@arianagrande",
+        "@kimkardashian",
+        "@ladygaga",
+        "@nickiminaj",
+        "@jlo",
+        "@badgalriri",
+        "@nasa",
+    )
+}
