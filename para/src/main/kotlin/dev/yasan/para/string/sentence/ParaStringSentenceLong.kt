962fdca32e260f85730479aba5ccff4bcf13ad22
Yasan <yasan@yasan.dev>
2022-09-15 14:25:53 +0430

    Create sentence preview providers

---
+++ para/src/main/kotlin/dev/yasan/para/string/sentence/ParaStringSentenceLong.kt
@@ -0,0 +1,18 @@
+package dev.yasan.para.string.sentence
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class ParaStringSentenceLong : PreviewParameterProvider<String> {
+    override val values: Sequence<String> = sequenceOf(
+        "The child’s favorite Christmas gift was the large box her father’s lawnmower came in.",
+        "Strawberries must be the one food that doesn't go well with this brand of paint.",
+        "That must be the tenth time I've been arrested for selling deep-fried cigars.",
+        "The hummingbird's wings blurred while it eagerly sipped the sugar water from the feeder.",
+        "People who insist on picking their teeth with their elbows are so annoying!",
+        "He was surprised that his immense laziness was inspirational to others.",
+        "I thought red would have felt warmer in summer but I didn't think about the equator.",
+        "Trash covered the landscape like sprinkles do a birthday cake.",
+        "As you consider all the possible ways to improve yourself and the world, you notice John Travolta seems fairly unhappy.",
+        "Writing a list of random sentences is harder than I initially thought it would be.",
+    )
+}
---
+++ para/src/main/kotlin/dev/yasan/para/string/sentence/ParaStringSentenceShort.kt
@@ -0,0 +1,18 @@
+package dev.yasan.para.string.sentence
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class ParaStringSentenceShort : PreviewParameterProvider<String> {
+    override val values: Sequence<String> = sequenceOf(
+        "He colored deep space a soft yellow.",
+        "Her fragrance of choice was fresh garlic.",
+        "Tom got a small piece of pie.",
+        "It must be five o'clock somewhere.",
+        "So long and thanks for the fish.",
+        "I hear that Nancy is very pretty.",
+        "Love is not like pizza.",
+        "Iguanas were falling out of the trees.",
+        "We should play with legos at camp.",
+        "I'd rather be a bird than a fish.",
+    )
+}
