147d5460f8d8b76287e7f285554248d7daae599c
Yasan <yasan@yasan.dev>
2022-09-16 10:39:07 +0430

    Create more preview providers

---
+++ para/src/main/kotlin/dev/yasan/para/string/email/ParaStringEmail.kt
@@ -0,0 +1,18 @@
+package dev.yasan.para.string.email
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class ParaStringEmail : PreviewParameterProvider<String> {
+    override val values: Sequence<String> = sequenceOf(
+        "Oliver.Orpha@hotmail.com",
+        "JeannetteN@gmail.com",
+        "jarredjonie96@yandex.ru",
+        "benjy3246@mail.net",
+        "P.Libbie@ent.biz",
+        "AntonyAntony@gmail.com",
+        "Carsonidella2@yahoo.com",
+        "j.clarence@metamail.com",
+        "Tamelar@gmail.com",
+        "JonathonTabby@yandexmail.ru",
+    )
+}
---
+++ para/src/main/kotlin/dev/yasan/para/string/lorem/ParaStringLoremParagraphShort.kt
@@ -0,0 +1,18 @@
+package dev.yasan.para.string.lorem
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class ParaStringLoremParagraphShort : PreviewParameterProvider<String> {
+    override val values: Sequence<String> = sequenceOf(
+        "Lorem ipsum dolor sit amet. Aut harum laboriosam ut numquam sequi aut inventore dicta aut voluptatem vero est aperiam molestias. Ab harum voluptatem sit dolor consequatur et molestias nostrum non consequatur quas.",
+        "Lorem ipsum dolor sit amet. Eum quia voluptatem aut voluptatibus galisum sit dolorum labore est velit possimus quo aspernatur quasi. Non cumque eligendi eum dolorem provident rem commodi labore eum quas iste ut earum consequatur eum commodi voluptatum. Et dolor odit sed consequatur similique nam ullam corrupti aut quis sint eos consequuntur debitis est adipisci inventore.",
+        "Lorem ipsum dolor sit amet. Et tempora error et magnam voluptate est necessitatibus modi aut sequi alias sed temporibus accusamus. Ea repellendus consequatur et internos harum eum officiis ducimus qui obcaecati illo ut incidunt tempore et modi dolores qui voluptates maiores. Aut iusto magnam et perferendis magnam ut maxime nostrum sed laudantium magni. Aut recusandae sequi sit atque impedit non beatae voluptatibus sit quas tenetur ut suscipit repellat ut voluptatibus eligendi aut architecto dolorem?",
+        "Lorem ipsum dolor sit amet. Et consectetur doloribus aut nulla galisum eum aliquid soluta eos nobis quia qui dolor facilis. Qui voluptatem dolorem id ullam itaque aut accusantium nisi aut excepturi eaque sed esse reprehenderit.",
+        "Lorem ipsum dolor sit amet. Sit perspiciatis doloribus sed exercitationem quas aut amet internos et voluptatem expedita quo  numquam est quibusdam beatae aut commodi laborum. Quo galisum aliquid eos illo porro qui deserunt perferendis qui omnis voluptatem.",
+        "Lorem ipsum dolor sit amet. Sit enim porro est iusto quisquam est recusandae natus et dolorem quis  harum dignissimos ad omnis natus? Rem quisquam culpa aut velit nobis est sunt voluptatibus.",
+        "Lorem ipsum dolor sit amet. Ut tenetur natus sed quia distinctio ex temporibus amet et doloribus dolore. Vel aliquam voluptates At quis veniam et perferendis dolores.",
+        "Lorem ipsum dolor sit amet. Aut enim sunt et porro omnis est architecto molestiae. Aut illo excepturi hic doloremque vitae eos suscipit minus cum assumenda exercitationem et dicta quaerat in sint obcaecati aut dolores porro. Rem libero corporis sit quidem Quis eos odio sint. In quod inventore non rerum quidem ea quaerat expedita id quisquam sunt.",
+        "Lorem ipsum dolor sit amet. Sed velit assumenda et libero impedit et rerum dolor et nostrum voluptatem qui aliquam tempora. Aut iusto delectus ea Quis totam ab iste consequuntur sed obcaecati sunt aut galisum impedit.",
+        "Lorem ipsum dolor sit amet. Ut voluptatum sequi et tenetur saepe in explicabo omnis. Est iure quis vel architecto magnam nam obcaecati saepe et quia rerum. Ea veniam consequatur sed officiis voluptatem sed tempore perspiciatis rem eius veniam id facilis voluptatem.",
+    )
+}
